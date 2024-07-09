9148386b2b30957376c7a6ec5555e46072a8f333
AGEN233 <39018590+AGEN233@users.noreply.github.com>
2024-07-09 15:25:48 +0800

    Update main.py

--- api/main.py
+++ api/main.py
@@ -46,7 +46,7 @@ app.add_middleware(
 )
 
 
-@app.get("/123", tags=["PUBLIC_API"], summary="返回完整统计信息")
+@app.get("/all", tags=["PUBLIC_API"], summary="返回完整统计信息")
 def all(start: int = 0, end: int = -1, rule: str = "updated"):
     """返回数据库统计信息和文章信息列表
     - start: 文章信息列表从 按rule排序后的顺序 的开始位置
